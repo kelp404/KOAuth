948a3852f159b5af299fc37ed4c05032eb2b7e7b
Kelp <kelp@phate.org>
2013-01-26 13:50:24 +0800

    clear up.

--- KOAuth/KOAuthDemoAppDelegate.h
+++ KOAuth/KOAuthDemoAppDelegate.h
@@ -3,7 +3,6 @@
 //  KOAuth
 //
 //  Created by Kelp on 2013/01/24.
-//  Copyright (c) 2013 Accuvally Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- KOAuthTests/KOAuthTests.m
+++ KOAuthTests/KOAuthTests.m
@@ -3,7 +3,6 @@
 //  KOAuthTests
 //
 //  Created by Kelp on 2013/01/24.
-//  Copyright (c) 2013 Accuvally Inc. All rights reserved.
 //
 
 #import "KOAuthTests.h"
